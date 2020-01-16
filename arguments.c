3d2a99807aac05464c6b60ccd3b14ead90f150cf
andre <toddot@live.ru>
2020-01-16 16:05:37 +0300

    Removed yet another useless folder

---
+++ arguments.c
@@ -0,0 +1,9 @@
+//
+//  arguments.c
+//  ddos2
+//
+//  Created by Andre Zay on 16/01/2020.
+//  Copyright © 2020 Andre Zay. All rights reserved.
+//
+
+#include "arguments.h"
---
+++ module.c
@@ -0,0 +1,9 @@
+//
+//  module.c
+//  ddos2
+//
+//  Created by Andre Zay on 16/01/2020.
+//  Copyright © 2020 Andre Zay. All rights reserved.
+//
+
+#include "module.h"
