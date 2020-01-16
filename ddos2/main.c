ab73d449b392b3d1517573f2cbcda885c54a7c4a
andre <toddot@live.ru>
2020-01-16 10:09:08 +0300

    Initial Commit

---
+++ ddos2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ddos2
+//
+//  Created by Andre Zay on 16/01/2020.
+//  Copyright © 2020 Andre Zay. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
