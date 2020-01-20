2576531eeb215871aee36c7ef9a037c9e2ed5181
andre <toddot@live.ru>
2020-01-20 14:57:32 +0300

    Network WIP, major bug fixed.

---
+++ network.c
@@ -0,0 +1,9 @@
+//
+//  network.c
+//  ddos2
+//
+//  Created by Andre Zay on 20/01/2020.
+//  Copyright © 2020 Andre Zay. All rights reserved.
+//
+
+#include "network.h"
---
+++ network.h
@@ -0,0 +1,14 @@
+//
+//  network.h
+//  ddos2
+//
+//  Created by Andre Zay on 20/01/2020.
+//  Copyright © 2020 Andre Zay. All rights reserved.
+//
+
+#ifndef network_h
+#define network_h
+
+#include <stdio.h>
+
+#endif /* network_h */
